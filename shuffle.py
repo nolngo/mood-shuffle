86099ef313541a7897ba2eb176880ee8e0e0ca82
Nolan <60010605+nolngo@users.noreply.github.com>
2020-10-15 02:04:16 -0700

    returned dictionary with multiple values per key

--- shuffle.py
+++ shuffle.py
@@ -1,6 +1,15 @@
 def load_playlist(filename):
     playlist = open(filename, "r")
+    mood_song = []
+    mood_playlist = {}
     for line in playlist:
-        line.split(",")
-        return playlist
-print(load_playlist("resources/beatles.txt"))
+        line = line.strip("\n")
+        mood_song = (line.split(","))
+        mood = mood_song[0]
+        song = mood_song[1]
+        if mood in mood_playlist:       #checking if mood(john) is already in a playlist, if it is
+            mood_playlist[mood].append(song)        #get the rest of lists with (john) as mood, and append it with other (john) songs.
+        else:       #else means that if john is not in the play list yet-
+            mood_playlist[mood] = [song]        #a new list of john songs is created and that list contains the first (john) song.
+    return mood_playlist
+print(load_playlist("beatles.txt"))
